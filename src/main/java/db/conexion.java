3ce7d5a10b9663e7095b36fc8f1acebdbbdcc17b
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-04-16 21:11:13 -0500

    se empieza a crear la clase donde se realiza la conexion con slq server

---
+++ src/main/java/db/conexion.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.sql.Connection;
+
+public class conexion {
+
+    Connection conexion=null;
+
+    String usuario="NicolasPena";
+    String contrasenia="nicolas";
+    String bd="Software2";
+    String Ip="localhost";
+}
