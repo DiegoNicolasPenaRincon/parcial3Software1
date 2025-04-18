a8717c70118219299c1064ed27583c70a4805b3d
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-04-18 04:53:16 -0500

    Se elimino una porcion de codigo innecesaria

--- src/main/java/co/edu/uniquindio/alquiler/model/ReciboPago.java
+++ src/main/java/co/edu/uniquindio/alquiler/model/ReciboPago.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 public class ReciboPago {
 
-    String nombreEstudiante;
+    String IDestudiante;
     EstadoRecibo estadoRecibo;
     LocalDate fechaExpedicion;
     LocalDate fechaPago;
@@ -15,8 +15,8 @@ public class ReciboPago {
     String nombreMateria;
     int numeroReferencia;
 
-    public ReciboPago(String nombreEstudiante, EstadoRecibo estadoRecibo, LocalDate fechaExpedicion, LocalDate fechaPago, LocalDate fechaVencimiento,String nombreMateria,int numeroReferencia) {
-        this.nombreEstudiante = nombreEstudiante;
+    public ReciboPago(String IDestudiante, EstadoRecibo estadoRecibo, LocalDate fechaExpedicion, LocalDate fechaPago, LocalDate fechaVencimiento,String nombreMateria,int numeroReferencia) {
+        this.IDestudiante = IDestudiante;
         this.estadoRecibo = estadoRecibo;
         this.fechaExpedicion = fechaExpedicion;
         this.fechaPago = fechaPago;
@@ -25,64 +25,5 @@ public class ReciboPago {
         this.numeroReferencia = numeroReferencia;
     }
 
-    public String getNombreEstudiante() {
-        return nombreEstudiante;
-    }
 
-    public void setNombreEstudiante(String nombreEstudiante) {
-        this.nombreEstudiante = nombreEstudiante;
-    }
-
-    public EstadoRecibo getEstadoRecibo() {
-        return estadoRecibo;
-    }
-
-    public void setEstadoRecibo(EstadoRecibo estadoRecibo) {
-        this.estadoRecibo = estadoRecibo;
-    }
-
-    public LocalDate getFechaExpedicion() {
-        return fechaExpedicion;
-    }
-
-    public void setFechaExpedicion(LocalDate fechaExpedicion) {
-        this.fechaExpedicion = fechaExpedicion;
-    }
-
-    public double getValorPagar() {
-        return valorPagar;
-    }
-
-    public LocalDate getFechaPago() {
-        return fechaPago;
-    }
-
-    public void setFechaPago(LocalDate fechaPago) {
-        this.fechaPago = fechaPago;
-    }
-
-    public LocalDate getFechaVencimiento() {
-        return fechaVencimiento;
-    }
-
-    public void setFechaVencimiento(LocalDate fechaVencimiento) {
-        this.fechaVencimiento = fechaVencimiento;
-    }
-
-
-    public String getNombreMateria() {
-        return nombreMateria;
-    }
-
-    public void setNombreMateria(String nombreMateria) {
-        this.nombreMateria = nombreMateria;
-    }
-
-    public int getNumeroReferencia() {
-        return numeroReferencia;
-    }
-
-    public void setNumeroReferencia(int numeroReferencia) {
-        this.numeroReferencia = numeroReferencia;
-    }
 }
