ddb7c7a038fc9cbff34ef6567985856de248c0e6
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-21 10:44:41 -0500

    Se creo la pantalla de carga

---
+++ src/main/java/co/edu/uniquindio/alquiler/controller/PantallaCargaController.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.alquiler.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+
+public class PantallaCargaController {
+    @FXML
+    Label cargandoLabel;
+    @FXML
+    ProgressIndicator cargandoProgressBar;
+
+    public void initialize() {
+
+    }
+}
